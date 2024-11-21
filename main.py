7b8e1320097b2c99960e24aad02b39235a7c90bc
takogonia <timurkogoniia2205@gmail.com>
2024-11-21 19:02:20 +0300

    add function cipher decrypt

--- main.py
+++ main.py
@@ -8,4 +8,28 @@ def hide_phone_number():
     print(hidden_num)
 
 
-hide_phone_number()
+def cipher_encrypt():
+    text = input('Введите текст, который хотите зашифровать: ')
+    height = int(input('Введите высоту шифра (это обязательно должны быть целые числа от 1 не включительно): '))
+    strings = [''] * height
+    direction = None
+    string = 0
+    for char in text:
+        strings[string] += char
+        if string == 0:
+            direction = 1
+        elif string == height - 1:
+            direction = -1
+        string += direction
+    enc_text = ''.join(strings)
+    print(enc_text)
+
+
+def cipher_decrypt():
+    enc_text = input('Введите текст, который хотите дешифровать: ')
+    height = int(input('Введите высоту шифра зашифрованного сообщения (это обязательно должны быть целые '
+                       'числа от 1 не включительно): '))
+    # хз пока
+
+
+cipher_encrypt()
